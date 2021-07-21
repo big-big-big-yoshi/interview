5d4c2d74971b0868ef34ddb1cc7f551dc2492bbc
Samuel Tesh <tesh@adobe.com>
2021-07-21 14:30:56 -0700

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <pivot/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import pivot from "./components/pivot.vue"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    pivot
   }
 }
 </script>
@@ -23,6 +22,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 10px;
 }
 </style>
---
+++ src/components/pivot.vue
@@ -0,0 +1,101 @@
+<template>
+  <div>
+    <div class="selection_container">
+      <div class="dimension_contianer">
+        <h3 style="text-align: left">Row Selection</h3>
+        <ul style="list-style-type: none; flex:1; text-align: left; padding: 0px">
+          <li
+            v-for="(item, index) in row_dimensions"
+            :key="item.value"
+            v-on:click="item.selected = !item.selected"
+            class="dim_selector"
+          >
+            <span v-if="item.selected">X-</span>
+            <span v-else>O-</span>
+            <span>{{ item.value }}</span>
+          </li>
+        </ul>
+      </div>
+      <div class="dimension_contianer">
+        <h3 style="text-align: left">Column Selection</h3>
+        <ul style="list-style-type: none; flex:1; text-align: left; padding: 0px">
+          <li
+            v-for="(item, index) in col_dimensions"
+            :key="item.value"
+            v-on:click="item.selected = !item.selected"
+          >
+            <span v-if="item.selected">X-</span>
+            <span v-else>O-</span>
+            <span>{{ item.value }}</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <!-- Pivot Table output goes here. -->
+
+    <!-- <pre style="text-align:left;">{{raw_data}}</pre> -->
+  </div>
+</template>
+
+<script>
+import source_data from "./data.js"
+export default {
+  name: "pivot",
+  props: {},
+  data: function () {
+    return {
+      row_dimensions: [
+        {
+          value: "region",
+          selected: false,
+        },
+        {
+          value: "sales_rep",
+          selected: false,
+        },
+        {
+          value: "item",
+          selected: false,
+        },
+        {
+          value: "item_category",
+          selected: false,
+        },
+      ],
+      col_dimensions: [
+        {
+          value: "region",
+          selected: false,
+        },
+        {
+          value: "sales_rep",
+          selected: false,
+        },
+        {
+          value: "item",
+          selected: false,
+        },
+        {
+          value: "item_category",
+          selected: false,
+        },
+      ],
+      raw_data: source_data
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.selection_container {
+  display: flex;
+}
+.dimension_contianer{
+    margin-right: 20px;
+}
+.dim_selector{
+    cursor:pointer;
+}
+</style>
